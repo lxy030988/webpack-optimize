39171aa6d1ee9ae91b9dd9d3fd94f000328be3f4
lixiangyu <654762937@qq.com>
2020-12-07 17:16:55 +0800

    optimize

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const title = "ssss";
+console.log(title);
+const $ = require("jquery");
+console.log($);
+//index源代码.replace('process.env.NODE_ENV','development');
+let a = "aaa";
+console.log("src/index.js process.env.NODE_ENV", process.env.NODE_ENV, a);
+console.log("ENVIRONMENT", ENVIRONMENT);
---
+++ src/title.js
@@ -0,0 +1,2 @@
+module.exports = "标题";
+require("bootstrap");
